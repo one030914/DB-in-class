1238a8027a32ada1d9c80e402dd543feabf9a3f2
GarryAdam <one030914@gmail.com>
2025-05-04 19:54:08 +0800

    DB

---
+++ week 10/hw/index.php
@@ -0,0 +1,7 @@
+<?php
+	echo "就讀系所：聯合大學資訊工程學系<br>";
+	echo "姓名：劉祐伸<br>";
+	echo "學號：U1224051<br>";
+?>
+<?php include("./assets/pages/course.html"); ?>
+<?php include("./assets/pages/links.html"); ?>
